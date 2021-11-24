a3ab49f4cfaa477ec392119530701b276be8ab6d
Heather Godbey <51833484+heathergodbey@users.noreply.github.com>
2021-11-23 21:03:22 -0500

    dark/light theme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import styled from 'styled-components';
-import StyledCard from './Components/Card';
+import styled, { ThemeProvider } from 'styled-components';
+import { useState } from 'react';
+
+// normally this goes in seperate file
+
+const BaseTheme = {
+  color: '#222',
+  background: '#fff',
+};
+
+const DarkTheme = {
+  color: '#fff',
+  background: '#222',
+};
+
+const Container = styled.div`
+  padding: 2rem;
+  color: ${(props) => props.theme.color};
+  background: ${(props) => props.theme.background};
+`;
 
 function App() {
+  const [baseTheme, setTheme] = useState(true);
+
+  const toggleTheme = () => {
+    setTheme(!baseTheme);
+  };
   return (
-    <div style={{ padding: '2rem' }}>
-      <StyledCard />
-    </div>
+    <ThemeProvider theme={baseTheme ? BaseTheme : DarkTheme}>
+      <Container>
+        <h1>hello :)</h1>
+        <button className='btn' onClick={toggleTheme}>
+          toggle me
+        </button>
+      </Container>
+    </ThemeProvider>
   );
 }
 
