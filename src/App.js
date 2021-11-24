68b51960a0ea4227692d97ebfb27b04178cc096a
Heather Godbey <51833484+heathergodbey@users.noreply.github.com>
2021-11-23 19:54:50 -0500

    card component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
 import styled from 'styled-components';
-import StyledTitle from './Components/StyledTitle';
-import { StyledButton, StyledExtendButton } from './Components/StyledButton';
-import { Button } from '@material-ui/core';
-import ComplexTitle from './Components/ComplexTitle';
-
-const StyledBtn = styled(Button)`
-  text-transform: capitalize;
-`;
+import StyledCard from './Components/Card';
 
 function App() {
   return (
-    <div>
-      {/* <StyledTitle special>Styled Component</StyledTitle>
-      <StyledTitle>Styled Component</StyledTitle>
-      <StyledButton>Click Me!</StyledButton>
-      <StyledExtendButton>Ext Btn</StyledExtendButton> */}
-      {/* <h1>Material UI</h1>
-      <Button color='primary' variant='contained'>
-        Click Me!
-      </Button>
-      <StyledBtn color='primary' variant='contained'>
-        Click Me!
-      </StyledBtn> */}
-      <ComplexTitle title='More Complex Title' />
+    <div style={{ padding: '2rem' }}>
+      <StyledCard />
     </div>
   );
 }
